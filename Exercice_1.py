1306b47fb6980cd40ad9064dc4ad0c837b221e6e
Norex-coder <theocharlotteau@gmail.com>
2024-10-14 18:49:53 +0000

    Yenamarre

--- Exercice_1.py
+++ Exercice_1.py
@@ -2,5 +2,10 @@
 
 # Demandez à l'utilisateur d'entrer une phrase.
 # Utilisez une boucle for pour parcourir chaque caractère de la phrase
-# et comptez le nombre de voyelles (a, e, i, o, u).
+# et comptez le nombre de voyelles (a, e, i, o, u, y).
 # Affichez le nombre total de voyelles trouvées.
+n= input()
+d= n.split()
+d = list(map(int, d))
+for tamere in (d):
+    ta gueule
